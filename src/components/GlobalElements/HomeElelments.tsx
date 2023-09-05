1c216d9dc0d8d37017ec46b43bc6eff394a34d74
ali nadi <nbali@alis-MacBook-Pro-2.local>
2023-09-05 09:13:13 +0330

    resposive for struct

--- src/components/GlobalElements/HomeElelments.tsx
+++ src/components/GlobalElements/HomeElelments.tsx
@@ -1,11 +1,21 @@
 'use client'
 
 import styled from "styled-components";
+import {devices} from "@/utils/constans";
 
 export const HomeDiv = styled.div`
-  padding: 3.5rem;
+  padding: 2rem;
   background-color: var(--background-color);
   color: var(--white-color);
+
+
+  @media only screen and ${devices.md} {
+    padding: 2.7rem;
+  }
+
+  @media only screen and ${devices.lg} {
+    padding: 3.5rem;
+  }
 `
 
 export const HomeSection = styled.section``
